0a152f335bad858231c66f339a05cfcac04c4ed4
_alfan <alfan@aleph-labs.com>
2025-04-23 14:46:30 +0700

    Initial Commit

---
+++ AiCare/AiCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiCareApp.swift
+//  AiCare
+//
+//  Created by Alfan on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AiCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AiCareTests/AiCareTests.swift
@@ -0,0 +1,17 @@
+//
+//  AiCareTests.swift
+//  AiCareTests
+//
+//  Created by Alfan on 23/04/25.
+//
+
+import Testing
+@testable import AiCare
+
+struct AiCareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
